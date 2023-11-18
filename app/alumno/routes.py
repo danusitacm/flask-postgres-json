f49368720109121fffab0de4a787a68a087e7d3b
danusitacm <danielacristaldomo@gmail.com>
2023-11-17 22:42:46 -0300

    add get post for alumno routes

--- app/alumno/routes.py
+++ app/alumno/routes.py
@@ -1,5 +1,22 @@
+from flask import request, jsonify
 from app.alumno import alumno
+from app.alumno.models import Alumno
+from app.extensions import db
 
-@alumno.route('/')
-def alumno_home():
-    return "<h1>alumnos Home</h1>"
+@alumno.route('/',methods=['GET','POST'])
+def obtener_alumnos():
+    if request.method == 'GET':
+        alumno = Alumno.query.all()
+        # Convertir los alumnos a JSON
+        return jsonify([a.to_json() for a in alumno])
+    elif request.method == 'POST':
+        nombre=request.json['nombre']
+        email=request.json['email']
+        telefono=request.json['telefono']
+        genero=request.json['genero']
+        puntaje_alumno=request.json['puntaje_alumno']
+        nuevo_alumno = Alumno(nombre,email,telefono,genero,puntaje_alumno)
+        db.session.add(nuevo_alumno)
+        db.session.commit()
+        return 200
+        
