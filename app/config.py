6f98d1b2a4ca9d5fb67f7872d5eeb8ba67840321
danusitacm <danielacristaldomo@gmail.com>
2023-11-17 22:41:19 -0300

    fix wrong sqlalchemy_uri

--- app/config.py
+++ app/config.py
@@ -2,5 +2,5 @@ import os
 
 SECRET_KEY = os.environ.get('SECRET_KEY')
 #postgres://username:password@localhost:5432/dbname
-SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'postgresql://postgres:password@localhost:5432/cars_api'
+SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:12345@localhost:5432/testdb'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
