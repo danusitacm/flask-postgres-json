9532f7f61edb17f4f9e2f8848f1c9d0192210a95
danusitacm <danielacristaldomo@gmail.com>
2023-11-20 00:30:57 -0300

    add  tutor model

---
+++ app/tutor/models.py
@@ -0,0 +1,20 @@
+from app.extensions import db 
+
+class Tutor(db.Model):
+    __tablename__='tutor'
+    tutor_pk=db.Column(db.Integer(), primary_key=True)
+    puntaje_tutor=db.Column(db.Float())
+    usuario_pk=db.Column(db.Integer, db.ForeignKey('usuario.usuario_pk'),nullable=False)
+    
+    # Relaciones 
+    usuario = db.relationship('Usuario', backref="tutores")
+    
+    
+    def __init__(self,puntaje_tutor,usuario_pk) -> None:
+        self.puntaje_tutor = puntaje_tutor
+        self.usuario_pk =usuario_pk
+
+    
+    def __repr__(self):
+        return '<Tutor_pk {}>'.format(self.tutor_pk)
+    
