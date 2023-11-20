b912cef856312b7c954fea212791de03d9f5f334
danusitacm <danielacristaldomo@gmail.com>
2023-11-20 00:40:01 -0300

    create class materiaschema

---
+++ app/materia/shcemas.py
@@ -0,0 +1,19 @@
+from .models import Materia
+from app.extensions import ma
+from marshmallow import fields,validate
+
+class MateriaSchema(ma.SQLAlchemySchema):
+    class Meta:
+        model = Materia
+        load_instance = True
+        fields = ("materia_pk", "nombre", "codigo")
+        ordered=True
+        
+    materia_pk = fields.Integer()
+    #para validar tengo que haces esto
+    # nombre = fields.String(required=True, validate=validate.Length(min=1, max=255),error=" el error")
+    nombre =fields.Float(required=True)
+    codigo = fields.Integer(required=True)
+
+materia_schema = MateriaSchema ()
+materias_schema  = MateriaSchema(many=True)
