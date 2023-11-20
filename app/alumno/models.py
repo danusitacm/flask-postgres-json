9d53db4024bb030cb8cd9114f96fd1ad544c246c
danusitacm <danielacristaldomo@gmail.com>
2023-11-20 00:30:03 -0300

    modify alumno model

--- app/alumno/models.py
+++ app/alumno/models.py
@@ -1,33 +1,20 @@
 from app.extensions import db 
-import json
 
 class Alumno(db.Model):
     __tablename__='alumno'
-    usuario_pk = db.Column(db.Integer, primary_key=True)
-    nombre = db.Column(db.String())
-    email = db.Column(db.String())
-    telefono = db.Column(db.String())
-    genero = db.Column(db.String())
-    alumno_pk=db.Column(db.Integer())
+    alumno_pk=db.Column(db.Integer(), primary_key=True)
     puntaje_alumno=db.Column(db.Float())
+    usuario_pk=db.Column(db.Integer, db.ForeignKey('usuario.usuario_pk'),nullable=False)
     
-
-    def __init__(self,nombre,email,telefono,genero,puntaje_alumno) -> None:
-        self.nombre = nombre
-        self.email = email
-        self.telefono = telefono
-        self.genero = genero
+    # Relaciones 
+    usuario = db.relationship('Usuario', backref="alumnos")
+    
+    
+    def __init__(self,puntaje_alumno,usuario_pk) -> None:
         self.puntaje_alumno = puntaje_alumno
-        
-    def to_json (self):
-        return { 'usuario_pk' : self.usuario_pk,
-                'nombre' : self.nombre,
-                'email' : self.email,
-                'telefono' : self.telefono,
-                'genero' : self.genero,
-                'alumno_pk' : self.alumno_pk,
-               'puntaje_alumno': self.puntaje_alumno }
+        self.usuario_pk =usuario_pk
+
     
     def __repr__(self):
-        return '<usuario_pk {}>'.format(self.usuario_pk)
+        return '<alumno_pk {}>'.format(self.alumno_pk)
     
