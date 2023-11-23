c3c7668ef3a69bf0f45e8583a6617039d70bc855
danusitacm <danielacristaldomo@gmail.com>
2023-11-23 19:57:16 -0300

    create solicitud model

---
+++ app/models/solicitud.py
@@ -0,0 +1,25 @@
+from app.extensions import db 
+
+
+class Solicitud(db.Model):
+    __tablename__='solicitud'
+    solicitud_pk = db.Column(db.Integer, primary_key=True)
+    fecha_solicitud = db.Column(db.Date)
+    estado_solicitud = db.Column(db.String(255))
+    materia_pk = db.Column(db.Integerdb.ForeignKey('materia_pk'),nullable=False)
+    alumno_pk = db.Column(db.Integer,db.ForeignKey('alumno_pk'),nullable=False)
+    
+    # Relaciones
+    alumno = db.relationship('Alumno', back_populates='solicitud')
+    materia = db.relationship('Materia', back_populates='solicitud')
+
+    def __init__(self,fecha_solicitud,estado_solicitud,materia_pk,alumno_pk) -> None:
+        self.fecha_solicitud = fecha_solicitud
+        self.estado_solicitud = estado_solicitud
+        self.materia_pk = materia_pk
+        self.alumno_pk = alumno_pk
+        
+
+    def __repr__(self):
+        return '<solicitud_pk {}>'.format(self.solicitud_pk)
+    
