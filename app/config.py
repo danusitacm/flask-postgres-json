88294c0eb1fac806a81a49aaf156e2550aa688e3
danusitacm <danielacristaldomo@gmail.com>
2023-11-23 19:42:04 -0300

    add .env

--- app/config.py
+++ app/config.py
@@ -1,6 +1,8 @@
-import os
+from os import environ 
+from dotenv import load_dotenv
+load_dotenv()
 
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = environ.get('SECRET_KEY')
 #postgres://username:password@localhost:5432/dbname
-SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:12345@localhost:5432/testdb'
+SQLALCHEMY_DATABASE_URI = environ.get('DATABASE_URL')
 SQLALCHEMY_TRACK_MODIFICATIONS = False
