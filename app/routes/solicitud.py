7e8f14b4bfbd85301e7775fe5e11dc33ac34c4cb
danusitacm <danielacristaldomo@gmail.com>
2023-11-23 20:57:00 -0300

    fix problem date

--- app/routes/solicitud.py
+++ app/routes/solicitud.py
@@ -3,6 +3,7 @@ from app.models.solicitud import Solicitud
 from app.extensions import db
 from app.schemas.solicitud import solicitud_schema, solicitudes_schema
 from marshmallow  import ValidationError
+from datetime import datetime
 from flask import Blueprint
 
 solicitud_bp = Blueprint('solicitud', __name__,url_prefix="/solicitudes")
@@ -49,18 +50,14 @@ def agregar_solicitud():
 def actualizar_solicitud(id):
     if request.method == 'PUT':
         try:
-            solicitud = solicitud.query.get(id)
+            solicitud = Solicitud.query.get(id)
             solicitud_schema.load(request.json)
-            fecha_solicitud = request.json['fecha_solicitud']
-            estado_solicitud = request.json['estado_solicitud']
-            materia_pk = request.json['materia_pk']
-            alumno_pk = request.json['alumno_pk']
-            solicitud_actualizada = Solicitud(fecha_solicitud,
-                                        estado_solicitud,
-                                        materia_pk,
-                                        alumno_pk)
+            solicitud.fecha_solicitud =  request.json['fecha_solicitud']
+            solicitud.estado_solicitud = request.json['estado_solicitud']
+            solicitud.materia_pk = request.json['materia_pk']
+            solicitud.alumno_pk = request.json['alumno_pk']
             db.session.commit()
-            return solicitud_schema.dump(solicitud_actualizada), 200
+            return solicitud_schema.dump(solicitud), 200
         except ValidationError as err:
             return err.messages,400
        
